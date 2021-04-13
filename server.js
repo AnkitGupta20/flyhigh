de4617a57f93e9408aa3f9f1b67b1133bea2cb36
AnkitGupta20 <akki719871@gmail.com>
2021-04-13 12:28:39 +0530

    heroku

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-app-heroku'));
+app.use(express.static('./dist/flyhigh/assets'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', { root: 'dist/flyhigh/assets' }),
+    res.sendFile('index.html', { root: 'dist/flyhigh/' }),
 );
 
 // Start the app by listening on the default Heroku port
